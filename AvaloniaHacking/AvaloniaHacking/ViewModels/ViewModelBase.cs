b03d225242275077321e14fd07d06a79d8333a55
Owen Cross <Owencross99@gmail.com>
2022-10-25 19:59:12 +0100

    lets go

---
+++ AvaloniaHacking/AvaloniaHacking/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaHacking.ViewModels {
+    public class ViewModelBase : ReactiveObject {
+    }
+}
---
+++ AvaloniaHacking/AvaloniaHacking/Views/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace AvaloniaHacking.Views {
+    public partial class MainWindow : Window {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
